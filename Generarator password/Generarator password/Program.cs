bb8585e3a30c7c841989c02ca9042b6616882853
anton <a.voskobovich@outlook.com>
2018-11-09 16:05:23 +0300

    1

--- Generarator password/Generarator password/Program.cs
+++ Generarator password/Generarator password/Program.cs
@@ -10,6 +10,24 @@ namespace Generarator_password
     {
         static void Main(string[] args)
         {
+            var rnd = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                string s = "";
+                s = GetLetter(rnd).ToString().ToUpper();
+                s += GetLetter(rnd);
+                s += GetLetter(rnd);
+
+                s += rnd.Next(10).ToString() + rnd.Next(10).ToString() + rnd.Next(10).ToString();
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+        public static char GetLetter(Random _random)
+        {
+            int num = _random.Next(0, 26); // Zero to 25
+            char let = (char)('a' + num);
+            return let;
         }
     }
-}class 000 
+}
