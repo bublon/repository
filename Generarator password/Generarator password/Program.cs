7cd8d264dffce8819dd1dd7d8bf73a0a5200b8ca
anton <a.voskobovich@outlook.com>
2018-11-09 15:55:38 +0300

    1

---
+++ Generarator password/Generarator password/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generarator_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}class 000 
