966e8934098e04bac3ef296d5a1b6b742eb84808
anton <a.voskobovich@outlook.com>
2018-11-14 14:00:59 +0300

    1

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+       public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return 0;
+        }
+    }
+}
